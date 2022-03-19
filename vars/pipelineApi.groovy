11f05bf0de20e88dba26b9f93e7bf980b98fee2b
xmhai <xmhai@hotmail.com>
2022-03-19 18:28:31 +0800

    jenkins

--- vars/pipelineApi.groovy
+++ vars/pipelineApi.groovy
@@ -36,7 +36,22 @@ def execute(Map configMap) {
     echo "Executing pipeline with the following stages, ${stageMap}"
 
     pipeline {
-        agent any
+        environment {
+            // This can be nexus3 or nexus2
+            NEXUS_VERSION = "nexus3"
+            // This can be http or https
+            NEXUS_PROTOCOL = "http"
+            // Where your Nexus is running
+            NEXUS_URL = "127.0.0.1:8081"
+            // Repository where we will upload the artifact
+            NEXUS_REPOSITORY = "maven-releases"
+            // Jenkins credential id to authenticate to Nexus OSS
+            NEXUS_CREDENTIAL_ID = "nexus-credentials"
+
+            HARBOR_URL = "192.168.86.43:9080/library"
+            HARBOR_CREDENTIAL_ID = "harbor-credentials"
+        }
+
         stages {
             stage("Clone") {
                 steps {
