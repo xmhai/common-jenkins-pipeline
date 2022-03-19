4cf3c6cae02c02186ac1929974e79c0ced38dc3a
xmhai <xmhai@hotmail.com>
2022-03-19 19:10:47 +0800

    test

--- vars/pipelineApi.groovy
+++ vars/pipelineApi.groovy
@@ -1,6 +1,6 @@
 #!groovy
 
-def call(Map configMap) {
+def execute(Map configMap) {
     echo "configMap: ${configMap}"
     application = configMap["application"]
     stageMap = []
@@ -37,10 +37,132 @@ def call(Map configMap) {
 
     pipeline {
         agent any
+        environment {
+            // This can be nexus3 or nexus2
+            NEXUS_VERSION = "nexus3"
+            // This can be http or https
+            NEXUS_PROTOCOL = "http"
+            // Where your Nexus is running
+            NEXUS_URL = "127.0.0.1:8081"
+            // Repository where we will upload the artifact
+            NEXUS_REPOSITORY = "maven-releases"
+            // Jenkins credential id to authenticate to Nexus OSS
+            NEXUS_CREDENTIAL_ID = "nexus-credentials"
+
+            HARBOR_URL = "192.168.86.43:9080/library"
+            HARBOR_CREDENTIAL_ID = "harbor-credentials"
+        }
+
         stages {
-            stage('Clone') {
+            stage("Clone") {
                 steps {
-                    echo "Clone..."
+                    script {
+                        echo "Clone..."
+                    }
+                }
+            }
+            stage("MavenBuild") {
+                steps {
+                    script {
+                        if (stageMap["MavenBuild"]) {
+                            echo "Maven Build..."
+                            sh "mvn clean install -DskipTests=true"
+                        }
+                    }
+                }
+            }
+            stage("NpmBuild") {
+                steps {
+                    script {
+                        if (stageMap["NpmBuild"]) {
+                            echo "Npm Build..."
+                            sh "npm install"
+                            sh "npm run build"
+                            
+                            echo "build frontend docker image..."
+                            def frontendImage = docker.build("${HARBOR_URL}/pfa-frontend", ".");
+                            echo "push frontend image to harbor..."
+                            docker.withRegistry("http://${HARBOR_URL}", "${HARBOR_CREDENTIAL_ID}") {
+                                frontendImage.push();
+                            }
+                        }
+                    }
+                }
+            }
+            stage("Push to Nexus") {
+                steps {
+                    script {
+                        if (stageMap["Push to Nexus"]) {
+                            echo "Push to Nexus..."
+                            def pomExists = fileExists "pom.xml"
+                            if (pomExists) {
+                                pom = readMavenPom file: "pom.xml";
+                                echo "${pom.artifactId}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}"
+
+                                if ("${pom.packaging}" == "pom") {
+                                        nexusArtifactUploader(
+                                            nexusVersion: NEXUS_VERSION,
+                                            protocol: NEXUS_PROTOCOL,
+                                            nexusUrl: NEXUS_URL,
+                                            groupId: pom.groupId,
+                                            version: pom.version,
+                                            repository: NEXUS_REPOSITORY,
+                                            credentialsId: NEXUS_CREDENTIAL_ID,
+                                            artifacts: [
+                                                [artifactId: pom.artifactId, classifier: '', file: "pom.xml", type: "pom"]
+                                            ]
+                                        );
+                                }
+
+                                if ("${pom.packaging}" == "jar") {
+                                    // Find built artifact under target folder
+                                    filesByGlob = findFiles(glob: "${f.name}/target/*.${pom.packaging}");
+                                    boolean exists = filesByGlob.length > 0
+
+                                    if (exists) {
+                                        // Print some info from the artifact found
+                                        echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
+                                        // Extract the path from the File found
+                                        artifactPath = filesByGlob[0].path;
+
+                                        nexusArtifactUploader(
+                                            nexusVersion: NEXUS_VERSION,
+                                            protocol: NEXUS_PROTOCOL,
+                                            nexusUrl: NEXUS_URL,
+                                            groupId: pom.groupId,
+                                            version: pom.version,
+                                            repository: NEXUS_REPOSITORY,
+                                            credentialsId: NEXUS_CREDENTIAL_ID,
+                                            artifacts: [
+                                                [artifactId: pom.artifactId, classifier: '', file: artifactPath, type: pom.packaging], 
+                                                [artifactId: pom.artifactId, classifier: '', file: "pom.xml", type: "pom"]
+                                            ]
+                                        );
+                                    }
+                                }
+                            } else {
+                                echo "${f.name}/pom.xml doesn't exist"
+                            }
+                        }
+                    }
+                }
+            }
+            stage("Deploy to Kubernetes") {
+                steps {
+                    script {
+                        if (stageMap["Deploy to Kubernetes"]) {
+                            echo "Deploy to Kubernetes..."
+                        }
+                    }
+                }
+            }
+            stage("Push to Harbor") {
+                steps {
+                    script {
+                        if (stageMap["Push to Harbor"]) {
+                            echo "Push to Harbor..."
+                        }
+                    }
                 }
             }
         }
