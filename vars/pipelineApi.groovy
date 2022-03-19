6a75430c8236ced8c2d0a80837a4527c291526a3
xmhai <xmhai@hotmail.com>
2022-03-19 12:43:00 +0800

    Jenkinsfile

--- vars/pipelineApi.groovy
+++ vars/pipelineApi.groovy
@@ -54,101 +54,113 @@ def execute(Map configMap) {
 
         stages {
             stage("Clone") {
-                script {
-                    echo "Clone..."
+                steps {
+                    script {
+                        echo "Clone..."
+                    }
                 }
             }
             stage("MavenBuild") {
-                script {
-                    if (stageMap["MavenBuild"]) {
-                        echo "Maven Build..."
-                        sh "mvn clean install -DskipTests=true"
+                steps {
+                    script {
+                        if (stageMap["MavenBuild"]) {
+                            echo "Maven Build..."
+                            sh "mvn clean install -DskipTests=true"
+                        }
                     }
                 }
             }
             stage("NpmBuild") {
-                script {
-                    if (stageMap["NpmBuild"]) {
-                        echo "Npm Build..."
-                        sh "npm install"
-                        sh "npm run build"
-                        
-                        echo "build frontend docker image..."
-                        def frontendImage = docker.build("${HARBOR_URL}/pfa-frontend", ".");
-                        echo "push frontend image to harbor..."
-                        docker.withRegistry("http://${HARBOR_URL}", "${HARBOR_CREDENTIAL_ID}") {
-                            frontendImage.push();
+                steps {
+                    script {
+                        if (stageMap["NpmBuild"]) {
+                            echo "Npm Build..."
+                            sh "npm install"
+                            sh "npm run build"
+                            
+                            echo "build frontend docker image..."
+                            def frontendImage = docker.build("${HARBOR_URL}/pfa-frontend", ".");
+                            echo "push frontend image to harbor..."
+                            docker.withRegistry("http://${HARBOR_URL}", "${HARBOR_CREDENTIAL_ID}") {
+                                frontendImage.push();
+                            }
                         }
                     }
                 }
             }
             stage("Push to Nexus") {
-                script {
-                    if (stageMap["Push to Nexus"]) {
-                        echo "Push to Nexus..."
-                        def pomExists = fileExists "pom.xml"
-                        if (pomExists) {
-                            pom = readMavenPom file: "pom.xml";
-                            echo "${pom.artifactId}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}"
+                steps {
+                    script {
+                        if (stageMap["Push to Nexus"]) {
+                            echo "Push to Nexus..."
+                            def pomExists = fileExists "pom.xml"
+                            if (pomExists) {
+                                pom = readMavenPom file: "pom.xml";
+                                echo "${pom.artifactId}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}"
 
-                            if ("${pom.packaging}" == "pom") {
-                                    nexusArtifactUploader(
-                                        nexusVersion: NEXUS_VERSION,
-                                        protocol: NEXUS_PROTOCOL,
-                                        nexusUrl: NEXUS_URL,
-                                        groupId: pom.groupId,
-                                        version: pom.version,
-                                        repository: NEXUS_REPOSITORY,
-                                        credentialsId: NEXUS_CREDENTIAL_ID,
-                                        artifacts: [
-                                            [artifactId: pom.artifactId, classifier: '', file: "pom.xml", type: "pom"]
-                                        ]
-                                    );
-                            }
-
-                            if ("${pom.packaging}" == "jar") {
-                                // Find built artifact under target folder
-                                filesByGlob = findFiles(glob: "${f.name}/target/*.${pom.packaging}");
-                                boolean exists = filesByGlob.length > 0
-
-                                if (exists) {
-                                    // Print some info from the artifact found
-                                    echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
-                                    // Extract the path from the File found
-                                    artifactPath = filesByGlob[0].path;
-
-                                    nexusArtifactUploader(
-                                        nexusVersion: NEXUS_VERSION,
-                                        protocol: NEXUS_PROTOCOL,
-                                        nexusUrl: NEXUS_URL,
-                                        groupId: pom.groupId,
-                                        version: pom.version,
-                                        repository: NEXUS_REPOSITORY,
-                                        credentialsId: NEXUS_CREDENTIAL_ID,
-                                        artifacts: [
-                                            [artifactId: pom.artifactId, classifier: '', file: artifactPath, type: pom.packaging], 
-                                            [artifactId: pom.artifactId, classifier: '', file: "pom.xml", type: "pom"]
-                                        ]
-                                    );
+                                if ("${pom.packaging}" == "pom") {
+                                        nexusArtifactUploader(
+                                            nexusVersion: NEXUS_VERSION,
+                                            protocol: NEXUS_PROTOCOL,
+                                            nexusUrl: NEXUS_URL,
+                                            groupId: pom.groupId,
+                                            version: pom.version,
+                                            repository: NEXUS_REPOSITORY,
+                                            credentialsId: NEXUS_CREDENTIAL_ID,
+                                            artifacts: [
+                                                [artifactId: pom.artifactId, classifier: '', file: "pom.xml", type: "pom"]
+                                            ]
+                                        );
                                 }
+
+                                if ("${pom.packaging}" == "jar") {
+                                    // Find built artifact under target folder
+                                    filesByGlob = findFiles(glob: "${f.name}/target/*.${pom.packaging}");
+                                    boolean exists = filesByGlob.length > 0
+
+                                    if (exists) {
+                                        // Print some info from the artifact found
+                                        echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
+                                        // Extract the path from the File found
+                                        artifactPath = filesByGlob[0].path;
+
+                                        nexusArtifactUploader(
+                                            nexusVersion: NEXUS_VERSION,
+                                            protocol: NEXUS_PROTOCOL,
+                                            nexusUrl: NEXUS_URL,
+                                            groupId: pom.groupId,
+                                            version: pom.version,
+                                            repository: NEXUS_REPOSITORY,
+                                            credentialsId: NEXUS_CREDENTIAL_ID,
+                                            artifacts: [
+                                                [artifactId: pom.artifactId, classifier: '', file: artifactPath, type: pom.packaging], 
+                                                [artifactId: pom.artifactId, classifier: '', file: "pom.xml", type: "pom"]
+                                            ]
+                                        );
+                                    }
+                                }
+                            } else {
+                                echo "${f.name}/pom.xml doesn't exist"
                             }
-                        } else {
-                            echo "${f.name}/pom.xml doesn't exist"
                         }
                     }
                 }
             }
             stage("Deploy to Kubernetes") {
-                script {
-                    if (stageMap["Deploy to Kubernetes"]) {
-                        echo "Deploy to Kubernetes..."
+                steps {
+                    script {
+                        if (stageMap["Deploy to Kubernetes"]) {
+                            echo "Deploy to Kubernetes..."
+                        }
                     }
                 }
             }
             stage("Push to Harbor") {
-                script {
-                    if (stageMap["Push to Harbor"]) {
-                        echo "Push to Harbor..."
+                steps {
+                    script {
+                        if (stageMap["Push to Harbor"]) {
+                            echo "Push to Harbor..."
+                        }
                     }
                 }
             }
